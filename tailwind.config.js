f4af966de1177c885b020d740e66f56ce2e74989
tegan jegede <tegan.jegede@yahoo.com>
2022-05-07 20:27:42 +0100

    tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,5 @@
 module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-    "./providers/**/*.{js,ts,jsx,tsx}",
-  ],
+  purge: ["./pages/**/*.js", "./components/**/*.js"],
   theme: {
     extend: {},
   },
